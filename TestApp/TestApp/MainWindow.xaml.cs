299342711132c4034c8c35ccfd118881fe0213e9
Dayle Chapman <daylejchapman@gmail.com>
2020-05-18 16:23:54 -0400

    Split decklist Log string

--- TestApp/TestApp/MainWindow.xaml.cs
+++ TestApp/TestApp/MainWindow.xaml.cs
@@ -26,13 +26,14 @@ namespace TestApp
         public MainWindow()
         {
             InitializeComponent();
-            var directory = new DirectoryInfo("S:\\AVAC\\Arena\\MTGArena\\MTGA_Data\\Logs\\Logs");
+            var dir = "D:\\MtG Arena\\MTGArena\\MTGA_Data\\Logs\\Logs\\";
+            var directory = new DirectoryInfo(dir);
             var myFile = (from f in directory.GetFiles()
                           orderby f.LastWriteTime descending
                           select f).First();
             Console.Out.WriteLine(myFile.Name);
             // Need to find [UnityCrossThreadLogger]<== Deck.GetDeckListsV3
-            string[] lines = System.IO.File.ReadAllLines("S:\\AVAC\\Arena\\MTGArena\\MTGA_Data\\Logs\\Logs\\" + myFile.Name);
+            string[] lines = System.IO.File.ReadAllLines(dir + myFile.Name);
             Console.Out.WriteLine(lines.Length);
             string deckIDLine = "Currently Empty";
             foreach (string s in lines)
@@ -44,7 +45,8 @@ namespace TestApp
                 }
             }
             Console.Out.WriteLine(deckIDLine);
-            //Regex r = new Regex();
+            string[] split = deckIDLine.Split(new string[]{"Deck.GetDeckListsV3 "},StringSplitOptions.None);
+            Console.Out.WriteLine(split[1]);
         }
     
     }
