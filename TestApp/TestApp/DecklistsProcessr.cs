672766fdb208dc6c87ac4c7525182508a5c18bd5
Graham <gvandeventer124@hotmail.com>
2020-05-18 19:03:14 -0400

    DeckCodes Added

---
+++ TestApp/TestApp/DecklistsProcessr.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace TestApp
+{
+    class DecklistsProcessr
+    {
+        public static Deck generateDeckCode(string jsonList)
+        {
+            Deck d = JsonConvert.DeserializeObject<Deck>(jsonList);
+            return d;
+        }
+    }
+}
--- TestApp/TestApp/MainWindow.xaml.cs
+++ TestApp/TestApp/MainWindow.xaml.cs
@@ -22,17 +22,19 @@ namespace TestApp
     /// </summary>
     public partial class MainWindow : Window
     {
-        
+        List<Deck> playerDecks;
         public MainWindow()
         {
             InitializeComponent();
-            var directory = new DirectoryInfo("S:\\AVAC\\Arena\\MTGArena\\MTGA_Data\\Logs\\Logs");
+            playerDecks = new List<Deck>();
+            var dir = "S:\\AVAC\\Arena\\MTGArena\\MTGA_Data\\Logs\\Logs\\";
+            var directory = new DirectoryInfo(dir);
             var myFile = (from f in directory.GetFiles()
                           orderby f.LastWriteTime descending
                           select f).First();
             Console.Out.WriteLine(myFile.Name);
             // Need to find [UnityCrossThreadLogger]<== Deck.GetDeckListsV3
-            string[] lines = System.IO.File.ReadAllLines("S:\\AVAC\\Arena\\MTGArena\\MTGA_Data\\Logs\\Logs\\" + myFile.Name);
+            string[] lines = System.IO.File.ReadAllLines(dir + myFile.Name);
             Console.Out.WriteLine(lines.Length);
             string deckIDLine = "Currently Empty";
             foreach (string s in lines)
@@ -43,9 +45,25 @@ namespace TestApp
                     break;
                 }
             }
-            Console.Out.WriteLine(deckIDLine);
-            //Regex r = new Regex();
+            // Console.Out.WriteLine(deckIDLine);
+            string[] split = deckIDLine.Split(new string[] { "\"payload\":[" }, StringSplitOptions.None);
+            split[1] = split[1].Substring(0, split[1].Length - 2);
+            //Console.Out.WriteLine(split[1]);
+            string[] deckjsons = split[1].Split(new string[] { "{\"commandZoneGRPIds\"" }, StringSplitOptions.None);
+
+            for (int i = 1; i < deckjsons.Length; i++)
+            {
+                Console.Out.WriteLine("{\"commandZoneGRPIds\"" + deckjsons[i]);
+                if (deckjsons[i].Substring(deckjsons[i].Length - 1) == ",")
+                {
+                    deckjsons[i] = deckjsons[i].Substring(0, deckjsons[i].Length - 1);
+                }
+                playerDecks.Add(DecklistsProcessr.generateDeckCode("{\"commandZoneGRPIds\"" + deckjsons[i]));
+            }
+            foreach (Deck d in playerDecks)
+            {
+                Console.Out.WriteLine("DeckCode: " + d.id);
+            }
         }
-    
     }
 }
